5ef56f3368d243d093d6b0f0e580d42956115a00
Ganesh Pise <piseganesh123@gmail.com>
2021-09-21 14:35:50 +0530

    Updated tf and shell scripts

---
+++ configure-ansible.tf
@@ -0,0 +1,45 @@
+// Create VM for Ansible host
+// Configure the Google Cloud provider
+
+variable "project_id" {
+  type = string
+  default = "vast-pad-319812"
+}
+
+provider "google" {
+// credentials = file("/home/piseg432/keys/gce-creator.json")
+// credentials =  $(GOOGLE_CLOUD_KEYFILE_JSON)
+ //project     = "vast-pad-319812"
+ project     = var.project_id
+ region      = "asia-south1"
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ // name         = "prografana-poc-vm-${random_id.instance_id.hex}"
+ name = "infra-mgr"
+ machine_type = "e2-small"
+ zone         = "asia-south1-c"
+ //tags = ["allow-grafana-ingress-3000", "allow-prometheus-ingress-9090"]
+ labels = {
+   "purpose" = "poc"
+   "preserve" = "no"
+ }
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-1804-bionic-v20210720"
+   }
+ }
+
+// 
+// metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync"
+metadata_startup_script = file("config-tools.sh")
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
